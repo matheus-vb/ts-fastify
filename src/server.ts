834c3fae0ce589004a2b0396d73879905f7f4dea
Matheus Batista <matheus.vbatista04@gmail.com>
2023-02-23 00:44:07 -0300

    feature: add session id migration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
 import { knex } from "./database";
 
 const app = fastify();
 
 app.get("/", async () => {
-    const tables = await knex("sqlite_schema").select("*");
+    const transaction = await knex("transactions").insert({
+        id: crypto.randomUUID(),
+        title: "Teste",
+        amount: 1,
+    }).returning("*");
 
-    return tables;
+    return transaction;
 });
 
 app.listen({
