618e89bf0a9bf3676ea2eb60bfb8f1dae4afa3d0
Matheus Batista <matheus.vbatista04@gmail.com>
2023-02-23 00:54:17 -0300

    feature: dotenv setup

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
 import { knex as setupKnex, Knex } from "knex";
+import "dotenv/config"
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("URL not found.");
+}
 
 export const config: Knex.Config = {
     client: "sqlite",
     connection: {
-        filename: "./db/app.db"
+        filename: `${process.env.DATABASE_URL}`
     },
     useNullAsDefault: true,
     migrations: {
