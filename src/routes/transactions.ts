a0d36403a994192136b54ab8e4989b3901d6622f
Matheus Batista <matheus.vbatista04@gmail.com>
2023-02-24 02:04:33 -0300

    feature: session id cookies middleware

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,29 +2,47 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { knex } from "../database";
 import crypto, { randomUUID } from "node:crypto";
+import { checkSessionIdExists } from "../middleware/check-session-id-exists";
 
 export async function transactionRoutes(app: FastifyInstance) {
-    app.get("/", async (request, reply) => {
-        const transactions = await knex("transactions").select("*");
+    app.get("/", {
+        preHandler: [checkSessionIdExists],
+    }, async (request, reply) => {
+        const { sessionId } = request.cookies
+
+        const transactions = await knex("transactions")
+        .where("session_id", sessionId)
+        .select();
 
         return reply.status(200).send({ data: transactions});
     })
 
-    app.get("/:id", async (request, reply) => {
+    app.get("/:id", {
+        preHandler: [checkSessionIdExists],
+    }, async (request, reply) => {
         const getTranssactionParamsSchema = z.object({
             id: z.string().uuid(),
         })
 
+        const { sessionId } = request.cookies;
         const { id } = getTranssactionParamsSchema.parse(request.params);
 
-        const transaction = await knex("transactions").where("id", id).first();
+        const transaction = await knex("transactions").where({
+            id,
+            session_id: sessionId
+        }).first();
 
         return reply.status(200).send({ data: transaction });
     })
     
-    app.get("/summary", async (request, reply) => {
+    app.get("/summary", {
+        preHandler: [checkSessionIdExists],
+    }, async (request, reply) => {
+        const { sessionId } = request.cookies;
+        
         const summary = await knex("transactions")
         .sum("amount", { as: "amount"})
+        .where("session_id", sessionId)
         .first()
 
         return reply.status(200).send({ data: summary });
