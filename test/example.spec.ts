df885eb1a2f2ec071617e1fe9a6d1ff23bff8cd3
Matheus Batista <matheus.vbatista04@gmail.com>
2023-02-25 00:07:13 -0300

    feature: get transaction and summary routes tests

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -53,5 +53,71 @@ describe("Transaction routes", () => {
             })
         ])
     })
+
+    test("user can get a specific transactions", async () => {
+        const createNewTransactionResponse = await request(app.server)
+            .post("/transactions")
+            .send({
+                title: "new transaction",
+                amount: 1200,
+                type: "credit",
+            });
+
+        const cookies = createNewTransactionResponse.get("Set-Cookie")
+
+        const listTransactionsResponse = await request(app.server)
+            .get("/transactions")
+            .set("Cookie", cookies)
+
+        expect(listTransactionsResponse.statusCode).toEqual(200);
+
+        const transactionId = listTransactionsResponse.body.data[0].id;
+
+        const getTransactionResponse = await request(app.server)
+            .get(`/transactions/${transactionId}`)
+            .set("Cookie", cookies)
+
+        expect(getTransactionResponse.statusCode).toEqual(200);
+
+        expect(getTransactionResponse.body.data).toEqual(
+            expect.objectContaining({
+                title: "new transaction",
+                amount: 1200,
+            })
+        )
+    })
+
+    test("user can get summary", async () => {
+        const createNewTransactionResponse = await request(app.server)
+            .post("/transactions")
+            .send({
+                title: "debit transaction",
+                amount: 1200,
+                type: "debit",
+            });
+
+        const cookies = createNewTransactionResponse.get("Set-Cookie")
+
+        await request(app.server)
+            .post("/transactions")
+            .set("Cookie", cookies)
+            .send({
+                title: "credit transaction",
+                amount: 6800,
+                type: "credit",
+            });
+
+        const summaryResponse = await request(app.server)
+            .get("/transactions/summary")
+            .set("Cookie", cookies)
+
+        expect(summaryResponse.statusCode).toEqual(200);
+
+        expect(summaryResponse.body.data).toEqual(
+            expect.objectContaining({
+                amount: 5600,
+            })
+        )
+    })
 })
 
